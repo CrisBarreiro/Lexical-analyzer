d086ed903e4cc8caebc331d1bb6ebf7b1cabc789
Cristina S. Barreiro <cristina.sbarreiro@gmail.com>
2015-11-14 16:41:28 +0100

    Distinguir identificadores de palabras reservadas. Realizar inserciones

--- AnalizadorLexico/ALex.c
+++ AnalizadorLexico/ALex.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "flex.h"
+#include "Defs.h"
+#include "Tsim.h"
 #include "ALex.h"
 
 componenteLexico SIG_COMP_LEX() {
 	componenteLexico comp;
 	comp.id = yylex();
 	comp.lexema = strdup(yytext);
+	if (comp.id == IDENTIFIER) {
+		/*Si el lexema es una secuencia alfanumérica, comprobamos si efectivamente es un identificador
+		 * o si es una palabra reservada del lenguaje*/
+		comp.id = NUEVA_ENTRADA(comp.lexema);
+	}
 	return comp;
 }
